3ce02999a3b4991e64e952553a4aa90bb3fe90a8
Extordy8 <victorjv8@hotmail.com>
2022-08-19 20:24:51 -0500

    template

--- proyecto/routes/web.php
+++ proyecto/routes/web.php
@@ -11,8 +11,8 @@ use Illuminate\Support\Facades\Route;
  * Route::put   | Actualizar
  */
 Route::get('/', function () {
-    return 'Ruta home';
-});
+    return view('home');
+})->name('home');
 
 Route::get('blog', function () {
     //consulta base de datos nivel rutas 
@@ -21,14 +21,14 @@ Route::get('blog', function () {
         ['id' =>2, 'title' => 'Laravel',    'slug' => 'laravel']
     ];
     return view('blog', ['posts' => $posts]);
-});
+})->name('blog');
 
 Route::get('blog/{slug}', function ($slug) {
     //consulta base de datos nivel rutas 
     $post = $slug;
 
     return view('post' , ['post' => $post]);
-});
+})->name('post');
 
 Route::get('buscar', function (Request $request) {
     return $request->all();
